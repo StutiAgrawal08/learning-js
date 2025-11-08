6a1c3645cf01a553a477e71e339cb5202bff410b
StutiAgrawal08 <168265889+StutiAgrawal08@users.noreply.github.com>
2025-11-08 08:56:35 +0000

    your message

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) //we are using node js not browser
+console.log(3+3);
+console.log("stuti")
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => value isn't assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "stuti");
+console.log(typeof null); //output => object
+console.log(typeof undefined); //output => undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const pie=3.14;
+let name="stuti";
+var age="20";
+address="mumbai";
+
+// pie= 5.4;
+// console.log(pie);
+let state
+name="sumit";
+age="21";
+address="delhi";
+console.table([name,age,address,state])
+
+// prefer not to use var coz of the issue in block scope and functional scope
